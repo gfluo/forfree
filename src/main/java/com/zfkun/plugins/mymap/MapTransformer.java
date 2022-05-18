96904d0979de3e8ec8a04e95661074b56d898b5c
罗 贵丰 <luoguifeng@cmiot.chinamobile.com>
2022-05-18 17:27:14 +0800

    Update

--- src/main/java/com/zfkun/plugins/mymap/MapTransformer.java
+++ src/main/java/com/zfkun/plugins/mymap/MapTransformer.java
@@ -23,7 +23,7 @@ public class MapTransformer implements MyTransformer {
     }
 
     public String getHookClassName() {
-        return "com/jetbrains/ls/data/ProductData";
+        return "com/jetbrains/ls/requests/ValidateKeyRequest";
     }
 
     public byte[] transform(String className, byte[] classBytes, int order) throws Exception {
