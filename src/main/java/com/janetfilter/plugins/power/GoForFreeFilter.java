4b20c179ea22a74a3e0f9f49f6016a8addfdc073
罗 贵丰 <luoguifeng@cmiot.chinamobile.com>
2022-05-23 11:57:06 +0800

    Update: 配置修改

--- src/main/java/com/janetfilter/plugins/power/GoForFreeFilter.java
+++ src/main/java/com/janetfilter/plugins/power/GoForFreeFilter.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class GoForFreeFilter {
     public static Byte[] testFilter(Byte[] encode) {
         System.out.println("222222222222222222222222222");
-        System.out.println(String.valueOf(encode));
+        // System.out.println(String.valueOf(encode));
         return encode;
     }
 }
