e7cc0c89daddfca6767507b060456c719a58fe25
罗 贵丰 <luoguifeng@cmiot.chinamobile.com>
2022-05-23 14:03:41 +0800

    Update: 配置修改

--- src/main/java/com/janetfilter/plugins/power/GoForFreeFilter.java
+++ src/main/java/com/janetfilter/plugins/power/GoForFreeFilter.java
@@ -1,11 +1,12 @@
 package com.janetfilter.plugins.power;
 
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 
 public class GoForFreeFilter {
-    public static Byte[] testFilter(Byte[] encode) {
+    public static byte[] testFilter(byte[] encode) {
         System.out.println("222222222222222222222222222");
         // System.out.println(String.valueOf(encode));
-        return encode;
+        return "fuck our".getBytes(StandardCharsets.UTF_8);
     }
 }
